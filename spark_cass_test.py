bbef0518e14160db639525c62e939c9daa80e63f
Rafał <rafal.morawski@protonmail.com>
2021-07-16 12:48:23 +0200

    c65

--- spark_cass_test.py
+++ spark_cass_test.py
@@ -1,7 +1,7 @@
 import os
 
 import cassandra as cass
-
+import shutil
 from cassandra.cluster import Cluster, Session
 import pandas as pd
 from pyspark.sql import SparkSession
@@ -17,33 +17,48 @@ def pandas_factory(colnames, rows):
 
 
 if __name__ == "__main__":
-    os.makedirs("/tmp/warehouse")
-    # spark = SparkSession \
-    #     .builder \
-    #     .appName("CassTest") \
-    #     .getOrCreate()
 
-    cluster = Cluster("192.168.55.16", connect_timeout=60)
+    cat = 'store_sales'
+    os.makedirs("tmp/warehouse", exist_ok=True)
+    spark = SparkSession \
+        .builder \
+        .appName("CassTest") \
+        .getOrCreate()
+
+    cluster = Cluster(["192.168.55.16"], connect_timeout=20)
     session: Session = cluster.connect()
     session.row_factory = pandas_factory
 
-    statement = "SELECT * FROM tpc_ds.warehouse;"
+    statement = f"SELECT * FROM tpc_ds.{cat};"
     ex = session.execute(statement, timeout=120)
-    df = ex._current_rows
-    i = 0
+    df: pd.DataFrame = ex._current_rows
+    df['d_date'] = df['d_date'].astype('string')
+    i = 1
+    df.to_parquet(f"./tmp/{cat}{i}.parquet")
+    sum_rows = df.count()
+
     while ex.has_more_pages:
-        ex.fetch_next_page()
-        df.append(ex._current_rows).to_parquet(f"/tmp/warehouse{i}.parquet")
         i += 1
+        ex.fetch_next_page()
+        df: pd.DataFrame = ex._current_rows
+        df['d_date'] = df['d_date'].astype('string')
+        df.to_parquet(f"./tmp/{cat}{i}.parquet")
+        sum_rows += df.count()
+
 
     # df = spark.read.format("org.apache.spark.sql.cassandra").options(table="catalog_sales", keyspace="tpc_ds").load()
     # x = spark.sql("SELECT count(*) FROM tpc_ds.warehouse")
 
 
-    # os.rmdir(".tmp")
 
 
+    print("Sum rows:", sum_rows)
+    mergedDF = spark.read.option("mergeSchema", "true").parquet(f"tmp/{cat}*")
+    mergedDF.createOrReplaceTempView(f"{cat}")
+    sqlDF = spark.sql("SELECT count(*), sum('ss_net_profit') FROM people")
+    sqlDF.show()
 
+    shutil.rmtree("./tmp", ignore_errors=True)
 
 
 
