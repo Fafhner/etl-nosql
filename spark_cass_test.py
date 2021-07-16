c65122e36eea3d53cf99126a95bb4a1697c1101e
Rafał <rafal.morawski@protonmail.com>
2021-07-16 22:31:36 +0200

    c66

--- spark_cass_test.py
+++ spark_cass_test.py
@@ -1,14 +1,13 @@
 import os
 
-import cassandra as cass
+import timeit
+import shutil
+import pandas as pd
+
 import shutil
 from cassandra.cluster import Cluster, Session
 import pandas as pd
 from pyspark.sql import SparkSession
-from pyspark import SparkContext
-from pyspark.sql.functions import explode
-from pyspark.sql.functions import split
-import pyspark
 
 
 def pandas_factory(colnames, rows):
@@ -17,38 +16,45 @@ def pandas_factory(colnames, rows):
 
 
 if __name__ == "__main__":
-
     cat = 'store_sales'
-    os.makedirs("tmp/warehouse", exist_ok=True)
+
+    os.makedirs("tmp/", exist_ok=True)
     spark = SparkSession \
-        .builder \
-        .appName("CassTest") \
+        .builder\
+        .master("spark://192.168.55.11:7077") \
+        .appName("CassTest")\
         .getOrCreate()
 
     cluster = Cluster(["192.168.55.16"], connect_timeout=20)
     session: Session = cluster.connect()
     session.row_factory = pandas_factory
 
-    statement = f"SELECT * FROM tpc_ds.store_sales;"
+    statement = f"SELECT * FROM tpc_ds.{cat};"
     ex = session.execute(statement, timeout=120)
     df: pd.DataFrame = ex._current_rows
     i = 1
-    df.to_parquet(f"./tmp/store_sales{i}.parquet")
-    sum_rows = df.count()
+    x = spark.createDataFrame(df)
+    x.write.parquet(f"./tmp/{cat}_{i}.parquet", mode='overwrite')
 
     while ex.has_more_pages:
         i += 1
         ex.fetch_next_page()
         df: pd.DataFrame = ex._current_rows
-        df.to_parquet(f"./tmp/store_sales{i}.parquet")
-        sum_rows += df.count()
+        if df.shape[0] != 0:
+            x = spark.createDataFrame(df, verifySchema=False)
+            x.write.parquet(f"./tmp/{cat}_{i}.parquet", mode='overwrite')
 
 
+    start = timeit.timeit()
     # print("Sum rows:", sum_rows)
-    # mergedDF = spark.read.option("mergeSchema", "true").parquet(f"tmp/{cat}*")
-    # mergedDF.createOrReplaceTempView(f"{cat}")
-    # sqlDF = spark.sql("SELECT count(*), sum('ss_net_profit') FROM people")
-    # sqlDF.show()
+    mergedDF = spark.read.option("mergeSchema", "true").parquet(f"tmp/{cat}*")
+    mergedDF.createOrReplaceTempView(f"{cat}")
+    sqlDF = spark.sql(f"SELECT count(*), sum('ss_net_profit') FROM {cat} GROUP BY 'ss_sold_date_sk'")
+    sqlDF.show()
+    sqlDF = spark.sql(f"SELECT * FROM {cat}")
+    sqlDF.show()
+    end = timeit.timeit()
+    print(f"Czas {end - start}")
 
     shutil.rmtree("./tmp", ignore_errors=True)
 
