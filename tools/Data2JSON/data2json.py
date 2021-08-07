0a5a531f5a02c615383b1a06bf38c6b640c185ea
Rafał <rafal.morawski@protonmail.com>
2021-08-07 21:17:57 +0200

    c83.5

--- tools/Data2JSON/data2json.py
+++ tools/Data2JSON/data2json.py
@@ -6,20 +6,21 @@ def line_to_json(line: str, schema):
     line_split = line.strip().split('|')
 
     schema_cols = schema['cols']
-    line_json = dict()
+    line_json = []
     for i, split in enumerate(line_split):
         if split != '':
-            col = schema_cols[i]
-            if col['type'] in ['varchar', 'ascii', 'date']:
-                line_json[col['col_name']] = f'"{split}"'
+            col_type = schema_cols[i]['type']
+            col_name = schema_cols[i]['col_name']
+            if col_type in ['varchar', 'ascii', 'date']:
+                line_json.append(f'"{col_name}": "{split}"')
             else:
-                line_json[col['col_name']] = f'{split}'
-    return line_json
+                line_json.append(f'"{col_name}": {split}')
+    return "{" + ", ".join(line_json) + "}"
 
 
 def dump_data(path, data):
     with open(path, 'w') as data_json_file:
-        json.dump({"data": data}, data_json_file)
+        data_json_file.write(f'"data": [{data}]')
         print(f"Created {p}")
 
 
@@ -39,7 +40,7 @@ if __name__ == '__main__':
                 schema = json.load(schema_json)
 
             os.makedirs(f"{json_write_path}/{data_size}/{table}", exist_ok=True)
-            with open(f"{data_path}/{data_size}/{table}.dat", 'r') as data_file:
+            with open(f"{data_path}/{data_size}/{table}.dat", 'r', encoding="ISO-8859-1") as data_file:
                 line = data_file.readline()
                 lines = []
                 chunk_id = 0
@@ -50,12 +51,9 @@ if __name__ == '__main__':
                         line = data_file.readline()
                     else:
                         p = f"{json_write_path}/{data_size}/{table}/{table}_{chunk_id}.json"
-                        dump_data(p, lines)
+                        dump_data(p, "\n".join(lines))
                         chunk_id += 1
                         lines = []
 
                 with open(f"{json_write_path}/{data_size}/{table}.info.json", 'w') as data_json_file:
                     json.dump({"table": table, "chunks": chunk_id}, data_json_file)
-
-
-
