1e9befeef45869acc2489c36217c3925a3c3183d
Rafał <rafal.morawski@protonmail.com>
2021-08-10 08:17:34 +0200

    c92.3

--- db/mongodb/etl/etl_process.py
+++ db/mongodb/etl/etl_process.py
@@ -32,7 +32,7 @@ def process(client: pm.MongoClient, udf: dict, spark: SparkSession):
         documents = collection.find(udf_val['filter'], udf_val['projection'], batch_size=BATCH_SIZE)
 
         while count_iter < iter_:
-            df: DataFrame = spark.createDataFrame([Row(**i) for i in list(documents[BATCH_SIZE*count_iter:BATCH_SIZE*count_iter+1])])
+            df = spark.createDataFrame([Row(**i) for i in list(documents[BATCH_SIZE*count_iter:BATCH_SIZE*count_iter+1])])
             files = f"./tmp/{udf['name']}_{udf_val['table_schema']}_{file_inc}.parquet"
             dataframes[f"{udf_val['table_schema']}"] = files
             df.write.parquet(files, mode='overwrite')
