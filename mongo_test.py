54f15b1fd436e21486f3d315545ff57d26127051
Rafał <rafal.morawski@protonmail.com>
2021-08-16 00:39:02 +0200

    c95

--- mongo_test.py
+++ mongo_test.py
@@ -1,13 +1,27 @@
 import pymongo as pm
 
+from pyspark import SparkConf
 
+from util.grid import create_scenarios
+import db.mongodb.etl.etl_process as etl
+from util import state
+import pymongo as pm
+from pyspark.sql import SparkSession
+import yaml
+from pyarrow import fs
+from timeit import default_timer as timer
 
 if __name__ == "__main__":
-    client = pm.MongoClient("192.168.55.20", 27017)
-    db = client['db']
-    date_dim_coll = db['date_dim']
+    spark = SparkSession \
+        .builder \
+        .appName("myApp") \
+        .getOrCreate()
 
-    x = date_dim_coll.find({}, None, batch_size=10000)
-
-
-    print(len(list(x[70000:90000])))
+    t1 = timer()
+    spark.read.format("mongo") \
+        .option("uri", "mongodb://127.0.0.1/db.date_dim") \
+        .load() \
+        .write.parquet("tmp2/date_dim.parquet")
+    t2 = timer()
+    
+    print(f"Time: {t2-t1}")
