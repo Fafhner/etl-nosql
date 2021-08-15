23ec476c9c3c9b3a0160cf3b12aaf1d14e073d55
Rafał <rafal.morawski@protonmail.com>
2021-08-16 01:00:14 +0200

    c97

--- db/mongodb/create_docker_compose.py
+++ db/mongodb/create_docker_compose.py
@@ -21,8 +21,9 @@ if __name__ == '__main__':
     cluster_size = int(sys.argv[1])
     path = sys.argv[2]
     data_size = int(sys.argv[3])
-
+    catalog = sys.argv[4]
     dc_json = load_from('docker-compose.yaml.json', path)
     parts = dc_json['parts'][0:cluster_size] + [dc_json['end']]
-    write_to('docker-compose.yaml', ("\n".join(parts)).format(data_size=data_size, cluster_size=cluster_size))
+    write_to('docker-compose.yaml', ("\n".join(parts)).format(data_size=data_size,
+                                                              cluster_size=cluster_size, mongo_catalog=catalog))
 
