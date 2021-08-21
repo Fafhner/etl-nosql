adadad8a6827acf4a33ea9f4f6c59d01d657d353
Rafał <rafal.morawski@protonmail.com>
2021-08-21 15:23:36 +0200

    c103

---
+++ scripts/gen-data.sh
@@ -0,0 +1,4 @@
+ansible-playbook -i /home/magisterka/etl-nosql/ansible-gen-data/hosts \
+ -u magisterka \
+ --extra-vars "ansible_become_password=$1, ansible_ssh_pass=$2" \
+ /home/magisterka/etl-nosql/ansible-gen-data/hosts/data_gen.yaml
--- tools/CassTbConv/config.py
+++ tools/CassTbConv/config.py
@@ -1,21 +1,28 @@
 import json
-
+import sys
 if __name__ == '__main__':
-    tables_dest = "../../db/table_data"
+    with open(sys.argv[1], 'r') as conf:
+        config_file = json.load(conf)
 
-    tables = ["catalog_returns", "date_dim", "store_sales", "catalog_sales", "web_sales", "warehouse", "customer"]
-    sizes = [2]
+    tables_data_src = config_file["tables_dest"]
+    tables_schema_src = config_file["tables_schema_src"]
+    outputDir = config_file["outputDir"]
+    namespace = config_file["namespace"]
+    out_file_name = config_file["out_file_name"]
+    tables = config_file["tables"]
+    sizes = config_file["sizes"]
 
     infos = []
     for s in sizes:
         for t in tables:
             x = {
-                "outputDir": "../../db/table_data/sstables",
-                "outputDirName": f"{s}/tpc_ds/{t}",
-                "dataSrcPath": f"{tables_dest}/{s}/{t}.dat",
+                "outputDir": outputDir,
+                "outputDirName": f"{s}/{namespace}/{t}",
+                "dataSrcPath": f"{tables_data_src}/{s}/{t}.dat",
+                "tableSchemaSrcPath": f"{tables_schema_src}/{t}.json",
                 "tableName": t
             }
             infos.append(x)
 
-    with open('config.json', 'w') as outfile:
+    with open(out_file_name, 'w') as outfile:
         json.dump({"infos": infos}, outfile, indent=4)
