d7874488d47347d7e4d997202da97de52bfc03e6
Rafał <rafal.morawski@protonmail.com>
2021-08-21 15:30:27 +0200

    c103.2

--- scripts/create-sstables.sh
+++ scripts/create-sstables.sh
@@ -3,4 +3,4 @@
 ansible-playbook -i /home/magisterka/etl-nosql/ansible-gen-data/hosts \
  -u magisterka \
  --extra-vars "ansible_become_password=$1, ansible_ssh_pass=$2" \
- /home/magisterka/etl-nosql/ansible-gen-data/hosts/genTables.yaml
+ /home/magisterka/etl-nosql/ansible-gen-data/sstables.yaml
--- scripts/gen-data.sh
+++ scripts/gen-data.sh
@@ -3,4 +3,4 @@
 ansible-playbook -i /home/magisterka/etl-nosql/ansible-gen-data/hosts \
  -u magisterka \
  --extra-vars "ansible_become_password=$1, ansible_ssh_pass=$2" \
- /home/magisterka/etl-nosql/ansible-gen-data/hosts/data_gen.yaml
+ /home/magisterka/etl-nosql/ansible-gen-data/data_gen.yaml
