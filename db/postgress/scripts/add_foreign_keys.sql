b9020415420b0bda31a6e4888b8926be62590c18
Rafał <rafal.morawski@protonmail.com>
2021-10-28 20:11:05 +0200

    f1.15

--- db/postgress/scripts/add_foreign_keys.sql
+++ db/postgress/scripts/add_foreign_keys.sql
@@ -18,17 +18,17 @@ ALTER TABLE catalog_sales ADD CONSTRAINT fk_catalog_sales_5 FOREIGN KEY (cs_ship
 ALTER TABLE catalog_sales ADD CONSTRAINT fk_catalog_sales_6 FOREIGN KEY (cs_warehouse_sk) REFERENCES warehouse (w_warehouse_sk);
 
 
-ALTER TABLE customer ADD CONSTRAINT fk_customer_1 FOREIGN KEY (c_current_addr_sk) REFERENCES customer_address (cs_order_number);
+ALTER TABLE customer ADD CONSTRAINT fk_customer_1 FOREIGN KEY (c_current_addr_sk) REFERENCES customer_address (ca_address_sk);
 ALTER TABLE customer ADD CONSTRAINT fk_customer_2 FOREIGN KEY (c_first_shipto_date_sk) REFERENCES date_dim (d_date_sk);
 ALTER TABLE customer ADD CONSTRAINT fk_customer_3 FOREIGN KEY (c_first_sales_date_sk) REFERENCES date_dim (d_date_sk);
 ALTER TABLE customer ADD CONSTRAINT fk_customer_4 FOREIGN KEY (c_last_review_date_sk) REFERENCES date_dim (d_date_sk);
 
 
 ALTER TABLE store_returns ADD CONSTRAINT fk_store_returns_1 FOREIGN KEY (sr_returned_date_sk) REFERENCES date_dim (d_date_sk);
-ALTER TABLE store_returns ADD CONSTRAINT fk_store_returns_2 FOREIGN KEY (sr_item_sk ) REFERENCES fk_ (ss_item_sk);
+ALTER TABLE store_returns ADD CONSTRAINT fk_store_returns_2 FOREIGN KEY (sr_item_sk ) REFERENCES store_sales (ss_item_sk);
 ALTER TABLE store_returns ADD CONSTRAINT fk_store_returns_3 FOREIGN KEY (sr_customer_sk) REFERENCES customer (c_customer_sk);
 ALTER TABLE store_returns ADD CONSTRAINT fk_store_returns_4 FOREIGN KEY (sr_addr_sk) REFERENCES customer_address (ca_address_sk);
-ALTER TABLE store_returns ADD CONSTRAINT fk_store_returns_5 FOREIGN KEY (sr_ticket_number ) REFERENCES fk_ (ss_ticket_number);
+ALTER TABLE store_returns ADD CONSTRAINT fk_store_returns_5 FOREIGN KEY (sr_ticket_number ) REFERENCES store_sales (ss_ticket_number);
 
 
 ALTER TABLE store_sales ADD CONSTRAINT fk_store_sales_1 FOREIGN KEY (ss_sold_date_sk) REFERENCES date_dim (d_date_sk);
