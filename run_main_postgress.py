9f70ab150a7b2366e4a97e065c84ec94ccbdfccb
Rafał <rafal.morawski@protonmail.com>
2021-10-29 13:58:53 +0200

    f3.5

--- run_main_postgress.py
+++ run_main_postgress.py
@@ -1,6 +1,6 @@
 import sys
 from datetime import datetime
-
+from time import sleep
 import db.postgress.etl.etl_process as etl
 from pyspark.sql import SparkSession
 from pyarrow import fs
@@ -33,14 +33,13 @@ if __name__ == "__main__":
         print("No arguments given.")
         file = ""
         udf = ""
-        loops = -1
+        idx = -1
         exit(-1)
     else:
         file = sys.argv[1]
         udf = sys.argv[2]
-        loops = int(sys.argv[3])
+        idx = int(sys.argv[3])
         output = sys.argv[4]
-
     conf = load_from_json(file)
 
     udf = load_from_json(udf, conf['udf_path'])
@@ -53,36 +52,35 @@ if __name__ == "__main__":
 
     spark = SparkSession \
         .builder \
-        .appName(f"Mongo_experiments_{datetime.now().strftime('%Y%m%d')}") \
+        .appName(f"Postgres_experiments_{datetime.now().strftime('%Y%m%d')}") \
         .getOrCreate()
 
     hdfs = fs.HadoopFileSystem('192.168.55.11', port=9000, user='magisterka')
 
     result_file = f"/home/magisterka/etl-nosql/result/run_etl_mongo.csv"
 
+    sleep(10)
     data_tries = dict()
-    idx = 0
     id_ = str(uuid.uuid4())
     udf['id'] = f"{str(datetime.now().date())}/{udf['name']}/{id_}"
 
-    while idx < loops:
-        udf['idx'] = idx
-        try:
-            result, result_df = etl.process(udf, spark)
-            hdfs.delete_dir_contents("./tmp")
-        except Exception as e:
-            omit_udf = True
-            logging.exception(e)
-            break
+    udf['idx'] = idx
+    try:
+        result, result_df = etl.process(udf, spark)
+        hdfs.delete_dir_contents("./tmp")
+    except Exception as e:
+        omit_udf = True
+        result = None
+        logging.exception(e)
+        exit(-1)
 
-        data_tries[idx] = result
-        idx += 1
-        a_data = f"{idx}," \
-                 f"{id_}," \
-                 f"{udf['name']}," \
-                 f"{params['cluster_size']}," \
-                 f"{params['data']}," \
-                 f"{params['o_mem']}," \
-                 f"{result['overall_time']}\n"
-        write_to(result_file, a_data, mode='a')
+    data_tries[idx] = result
+    idx += 1
+    a_data = f"{idx}," \
+             f"{id_}," \
+             f"{udf['name']}," \
+             f"{params['cluster_size']}," \
+             f"{params['data']},{params['o_mem']}," \
+             f"{result['overall_time']}\n"
+    write_to(result_file, a_data, mode='a')
 
